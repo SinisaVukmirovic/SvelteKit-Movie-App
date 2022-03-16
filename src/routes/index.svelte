8b36ff558c2bc7ec8fd6480ba30165921aeb05e2
SinisaVukmirovic <sinisavukmirovic@gmail.com>
2022-03-16 12:59:11 +0100

    Fetching data with TMDB API

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- to do a Server Side Rendered fetch request we use script with context="module" -->
+<script context="module">
+    export async function load({ fetch }) {
+        const apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=6022a4ae03ca3a0275414b787f65b604';
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+
+        if (res.ok) {
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
+</script> 
+
+<script>
+    export let popular;
+    console.log(popular)
+</script>
