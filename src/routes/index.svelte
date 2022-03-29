bb3359d13c8c1e0269b68eb304bda005c2e2b12f
SinisaVukmirovic <sinisavukmirovic@gmail.com>
2022-03-29 09:13:07 +0200

    Couldn't get environment vars to work...

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,12 @@
 <!-- to do a Server Side Rendered fetch request we use script with context="module" -->
 <script context="module">
+    // !!! can't get environment variables to work !!!
+    // import { MY_API_KEY } from '$lib/Env';
+    // console.log(MY_API_KEY);
+
     export async function load({ fetch }) {
-        const apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=6022a4ae03ca3a0275414b787f65b604';
+        const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=6022a4ae03ca3a0275414b787f65b604`;
+        // const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${MY_API_KEY}`;
         const res = await fetch(apiUrl);
         const data = await res.json();
 
